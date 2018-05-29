b42d94644c6ce029b438e16579f27054255afeb0
Manabu Matsui <manabu.matsui@gmail.com>
2018-05-29 15:10:58 +0900

    add AliasInterface

---
+++ lib/Model/AliasInterface.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: matsui
+ * Date: 2018/05/29
+ * Time: 12:08
+ */
+
+namespace Crhg\RemoClient\Model;
+
+
+interface AliasInterface
+{
+    public static function getType();
+}
